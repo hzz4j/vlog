4984c2f7a7921ff460e11a545fe22d680bb1a86b
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-11-26 06:07:29 +0800

    搭建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+/**
+ * vuepress 配置文件
+ */
+
+ module.exports = {
+    lang: 'zh-CN',
+    title: '静默的Vlog',
+    description: '静默的Vuepress Blog',
+  
+    themeConfig: {
+      logo: '/images/logo.png',
+    },
+  }
