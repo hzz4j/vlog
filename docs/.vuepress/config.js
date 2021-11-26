a90bceab202dccb089e61ba42792c291d049c7b3
zhuangzhuang-huang <cau1403090523@gmail.com>
2021-11-26 11:38:18 +0800

    update picture

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,12 +7,11 @@ const navbar = require('./config/navBar');
 const sidebar = autoGenSidebar();
 
 module.exports = {
+  base: '/vlog/',
   lang: 'zh-CN',
   title: '静默的Vlog',
   description: '静默的Vuepress Blog',
   head: [['link', { rel: 'icon', href: '/images/favicon-32x32.png' }]], // icon设置
-  base: '/vlog/',
-  
   themeConfig: {
     logo: '/images/logo.png',
     lastUpdated: false,   // 禁用显示更新时间
